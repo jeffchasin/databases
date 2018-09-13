f35db7adbae94b84642b4b9fd6b1b368aa8a221b
Jeff Chasin <jeff@jeffchasin.com>
2018-09-13 17:02:26 +0900

    module.exports.messages.post

--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,6 @@
 // defines the messages and users models that your app will use
 var connection = require('../db');
 
-// Test query outside of exports: (this works)
-// connection.query('SELECT * FROM messages', function (error, results, fields) {
-//   // error will be an Error if one occurred during the query
-//   // results will contain the results of the query
-//   // fields will contain information about the returned results fields (if any)
-//   if (error) {
-//     console.error('messages.get db.query error: ', error);
-//   }
-//   console.log('The [0] message is: ', results[0].txt);
-//   console.log('results: ', results);
-//   console.log('fields: ', fields);
-// });
 module.exports = {
   messages: {
     // a function which produces all the messages
@@ -24,7 +12,6 @@ module.exports = {
         if (error) {
           console.error('messages.get query error: ', error);
         }
-        // callback(results);
         callback(results);
         // results[0].txt -> Test message
         // console.log('The query results: ', JSON.stringify(results, null, 2)); ->
@@ -51,25 +38,34 @@ module.exports = {
       connection.end();
     },
     // a function which can be used to insert a message into the database
-    post: function (msg) {
-      connection.connect();
-      connection.query('INSERT INTO messages (txt, roomname, objectid, user_id) VALUES (' + msg + ', <ROOM>, <ORDERID>, <USERID>)', function (error, results, fields) {
+    // (from spec) request.json.username + request.json.message + request.json.roomname
+    // module.exports.messages.post(1, 'Test poster msg', 'office');
+    post: function (user, msg, room) {
+      connection.query('INSERT INTO messages (txt, roomname, user_id) VALUES (?, ?, ?)', [msg, room, user], function (error, results, fields) {
         if (error) {
           console.error('messages.post connection.query error: ', error);
         }
       });
-      // connection.end();
-      return results;
+      connection.end();
     }
   },
-  // TODO: YOU ARE HERE - test above, complete below.
   users: {
     // Ditto as above.
-    get: function () { },
+    get: function (callback) {
+      connection.query('SELECT * FROM users', function (error, results, fields) {
+        // error will be an Error if one occurred during the query
+        // results will contain the results of the query
+        // fields will contain information about the returned results fields (if any)
+        if (error) {
+          console.error('users.get query error: ', error);
+        }
+        callback(results);
+      });
+      connection.end();
+    },
+    // TODO:
     post: function () { }
   }
 };
 
-module.exports.messages.get(function(res) {
-  console.log(res[0].txt);
-});
+module.exports.messages.post(1, 'Test poster msg', 'office');
